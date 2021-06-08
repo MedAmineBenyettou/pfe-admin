4bb936196e97558e1e63abc08c34cc6743d548a7
MedAmineBenyettou <medamine.benyettou@gmail.com>
2021-06-08 19:56:05 +0100

    redux init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './css/App.css';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
- return <div className="App"></div>;
+ return (
+  <Provider store={store}>
+   <div className="App">App is working</div>
+  </Provider>
+ );
 };
 
 export default App;

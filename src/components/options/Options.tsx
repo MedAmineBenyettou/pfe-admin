56e71c6882ce919f23e82c6ac7e5724ce1eae35f
MedAmineBenyettou <medamine.benyettou@gmail.com>
2021-07-20 21:36:43 +0100

    *css fixed sidebar

--- src/components/options/Options.tsx
+++ src/components/options/Options.tsx
@@ -3,11 +3,22 @@ import { Link } from 'react-router-dom';
 import Admins from './Admins';
 import Profil from './Profil';
 import Laboratoire from './Laboratoire';
-
 import '../../css/options/Options.css';
 
 export const Options = () => {
  const [state, setState] = useState(<Profil />);
+
+ const handleClick = (
+  e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  c: any
+ ) => {
+  setState(c);
+  document.querySelectorAll('.option').forEach((e) => {
+   e.classList.remove('side-active');
+  });
+  //@ts-ignore
+  e.target.classList.toggle('side-active');
+ };
 
  return (
   <div className="options row">
@@ -15,27 +26,27 @@ export const Options = () => {
     <p className="col s12">Options</p>
     <Link
      to="#"
-     className="waves-effect waves-light"
-     onClick={() => {
-      setState(<Profil />);
+     className="waves-effect waves-light option side-active"
+     onClick={(e) => {
+      handleClick(e, <Profil />);
      }}
     >
      Profil
     </Link>
     <Link
      to="#"
-     className="waves-effect waves-light"
-     onClick={() => {
-      setState(<Admins />);
+     className="waves-effect waves-light option"
+     onClick={(e) => {
+      handleClick(e, <Admins />);
      }}
     >
      Admins
     </Link>
     <Link
      to="#"
-     className="waves-effect waves-light"
-     onClick={() => {
-      setState(<Laboratoire />);
+     className="waves-effect waves-light option"
+     onClick={(e) => {
+      handleClick(e, <Laboratoire />);
      }}
     >
      Laboratoire
